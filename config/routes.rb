f4ef19bdd3af8ccd819b496dd9b90e830c63251d
avyishi <avyishi@gmail.com>
2014-11-06 00:14:43 -0800

    fixed error in user model and updated layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Blocmarks::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :bookmarks
   resources :likes
 
