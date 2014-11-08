1da51c725fdb420ccc9c0cd4a109fa19c35bd6b3
avyishi <avyishi@gmail.com>
2014-11-08 00:11:31 -0800

    Created like model

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+#OmniAuth.config.logger = Rails.logger
+
+#Rails.application.config.middlewar.use OmniAuth::Builder do |
+ # provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  
+#end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104202909) do
+ActiveRecord::Schema.define(version: 20141108064532) do
 
   create_table "bookmarks", force: true do |t|
     t.string   "title"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20141104202909) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "likes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
