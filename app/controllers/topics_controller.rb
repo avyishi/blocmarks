38c2c77a56b914b3463f1f03a1a31e9cc4201431
avyishi <avyishi@gmail.com>
2014-12-04 01:31:46 -0800

    Fixed topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,39 +1,28 @@
 class TopicsController < ApplicationController
+
   def index
     @topics = Topic.all
     @like = Like.new
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+    @bookmarks = @topic.bookmarks
   end
 
   def new
     @topic = Topic.new
   end
 
-  def edit
-    @topic = Topic.find(params[:id])
-  end
-
-  def show
-    @topic = Topic.find(params[:id])
-  end
-
-  def create
+   def create
     @topic = Topic.find_by_name(params[:name])
-    if @topic.save
-      redirect_to @topic
-    else
-      render action: 'show'
+    unless @topic
+      @topic = Topic.new(name: params[:name])
+      @topic.save
     end
   end
 
-  def update
-    #if @topic.update
-     # redirect_to @topic, notice: 'Topic was successfully updated.'
-    #else
-     # render action: 'edit'
-   # end
-  end
-  
-  def destroy
+def destroy
     @topic = Topic.find(params[:id])
     name = @topic.name
 
@@ -45,4 +34,32 @@ class TopicsController < ApplicationController
       render :show
     end
   end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+     @topic = Topic.find(params[:id])
+    if @topic.update(topic_params)
+      redirect_to @topic, notice: 'Movie successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to @topic
+    else
+      render :new
+    end
+  end
+
+  private
+    def topic_params
+      params.require(:topic).permit(:name)
+    end
+
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,8 +2,5 @@ class Topic < ActiveRecord::Base
   has_many :bookmarks
   has_many :bookmarks, dependent: :destroy
 
-
-
-
   validates :name, length: {minimum: 5}, presence: true
 end
